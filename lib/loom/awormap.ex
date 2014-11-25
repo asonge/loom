8518eee906da62798175d841c678a53342d275fa
Alex Songe <a@songe.me>
2014-11-24 23:40:53 -0600

    Finished API port-over, must finish docs and specs next

--- lib/loom/awormap.ex
+++ lib/loom/awormap.ex
@@ -1,35 +1,39 @@
 defmodule Loom.AWORMap do
   @moduledoc """
-  An add-wins observed-remove map, based on optimized sets.
+  An add-wins observed-remove map or CRDTs, based on optimized sets.
+
+  Compose any CRDT that implements the `Loom.CRDT` protocol.
   """
 
   alias Loom.AWORMap, as: M
   alias Loom.AWORSet, as: Set
-  # alias Loom.Dots
 
-  #defstruct dots: Dots.new()
+  @type key :: term
+  @type actor :: term
+  @type crdt :: term
+  @type type :: atom | %{__struct__: atom}
+  @opaque t :: %M{
+    keys: Set.t,
+    values: %{key => crdt}
+  }
+
   defstruct keys: Set.new, values: %{}
 
+  @doc """
+  Returns a new AWORMap
+
+  The identity value of an empty AWORMap is `nil` because of the difficulties
+  of matching against `%{}`, which is not the equivalent of `[]`.
+
+      iex> Loom.AWORMap.new |> Loom.AWORMap.value
+      nil
+  """
+  @spec new() :: t
   def new, do: %M{}
 
-  def value(%M{values: values}), do: values
-
-  def get(map, key, %{__struct__: module}), do: get(map, key, module)
-  def get(%M{values: values}=map, key, module) do
-    for {k,mod} <- keys(map), k == key, mod == module do
-      values[{k,mod}]
-    end |> case do
-      [value] -> value
-      [] -> nil
-    end
-  end
-
-  def keys(%M{keys: set}), do: Set.value(set)
-
-  def has_key?(%M{keys: set}, key, module), do: Set.member?(set, {key, module})
-
-  def add(%M{}=map, actor, key, value), do: add({map, M.new}, actor, key, value)
-  def add({%M{keys: set, values: vals}=m, %M{keys: delta_set, values: delta_vals}=d}, actor, key, value) do
+  @spec put(t | {t,t}, actor, key, crdt) :: {t,t}
+  def put(%M{}=map, actor, key, value), do: put({map, M.new}, actor, key, value)
+  def put({%M{keys: set, values: vals}=m, %M{keys: delta_set, values: delta_vals}=d}, actor, key, value) do
     %{__struct__: struct_name} = value
     key_struct = {key, struct_name}
     {new_set, new_delta_set} = Set.add({set, delta_set}, actor, key_struct)
@@ -38,16 +42,18 @@ defmodule Loom.AWORMap do
     {%M{m|keys: new_set, values: new_values}, %M{d|keys: new_delta_set, values: new_delta_values}}
   end
 
-  def remove(%M{}=set, key, value), do: remove({set, M.new}, key, value)
-  def remove({%M{keys: set, values: vals}=m, %M{keys: delta_set, values: delta_vals}=d}, key, value) do
-    %{__struct__: struct_name} = value
-    key_struct = {key, struct_name}
+  @spec delete(t | {t,t}, key, type) :: {t,t}
+  def delete(set, key, %{__struct__: module}), do: delete(set, key, module)
+  def delete(%M{}=set, key, module), do: delete({set, M.new}, key, module)
+  def delete({%M{keys: set, values: vals}=m, %M{keys: delta_set, values: delta_vals}=d}, key, module) do
+    key_struct = {key, module}
     {new_set, new_delta_set} = Set.remove({set, delta_set}, key_struct)
     new_values = Map.delete(vals, key_struct)
     new_delta_values = Map.delete(delta_vals, key_struct)
     {%M{m|keys: new_set, values: new_values}, %M{d|keys: new_delta_set, values: new_delta_values}}
   end
 
+  @spec join(t, t) :: t
   def join(%M{keys: set1, values: values1}, %M{keys: set2, values: values2}) do
     new_set = Set.join(set1, set2)
     new_values = for key <- Set.value(new_set), into: %{} do
@@ -61,7 +67,118 @@ defmodule Loom.AWORMap do
     %M{keys: new_set, values: new_values}
   end
 
-  # defp match_keyspec({key, %{__struct__: s}}, key, %{__struct__: s}), do: true
-  # defp match_keyspec(_, _, _), do: false
+  def get(map, key, %{__struct__: module}), do: get(map, key, module)
+  def get(%M{values: values}=map, key, module) do
+    for {k,mod} <- keys(map), k == key, mod == module do
+      values[{k,mod}]
+    end |> case do
+      [value] -> value
+      [] -> struct(module)
+    end |> Loom.CRDT.value
+  end
+
+  def keys(%M{keys: set}), do: Set.value(set)
+
+  def has_key?(%M{keys: set}, key, module), do: Set.member?(set, {key, module})
+
+  def value(%M{values: values}) when map_size(values)==0, do: nil
+  def value(%M{values: values}) do
+    for {{k,_}, crdt} <- values, into: %{}, do: {k, Loom.CRDT.value(crdt)}
+  end
+
+end
+
+defimpl Loom.CRDT, for: Loom.AWORMap do
+
+  alias Loom.AWORMap, as: KVSet
+
+  @doc """
+  Returns a description of the operations that this CRDT takes.
+
+  Updates return a new CRDT, reads can return any natural datatype. This register
+  returns a value.
+  """
+  def ops(_crdt) do
+    [
+      update: [
+        delete: [:key, :value_type],
+        put: [:actor, :key, :value]
+      ],
+      read: [
+        get: [:value],
+        keys: [],
+        has_key: [],
+        value: []
+      ]
+    ]
+  end
+
+  @doc """
+  Applies a CRDT to a counter in an abstract way.
+
+  This is for ops-based support.
+
+      iex> alias Loom.CRDT
+      iex> alias Loom.LWWRegister
+      iex> bar = LWWRegister.new("bar")
+      iex> wtf = LWWRegister.new("wtf")
+      iex> Loom.AWORMap.new
+      ...> |> CRDT.apply({:put, :a, "foo", bar})
+      ...> |> CRDT.apply({:put, :a, "omg", wtf})
+      ...> |> CRDT.apply({:delete, "omg", LWWRegister})
+      ...> |> CRDT.apply(:value)
+      %{"foo" => "bar"}
+
+      iex> alias Loom.CRDT
+      iex> Loom.AWORMap.new |> CRDT.apply({:has_key, :none, Loom.LWWRegister})
+      false
+
+      iex> alias Loom.CRDT
+      iex> Loom.AWORMap.new |> CRDT.apply({:get, :none, Loom.LWWRegister})
+      nil
+
+      iex> alias Loom.CRDT
+      iex> Loom.AWORMap.new |> CRDT.apply(:keys)
+      []
+
+  """
+  def apply(crdt, {:put, actor, key, value}) do
+    {map, _} = KVSet.put(crdt, actor, key, value)
+    map
+  end
+  def apply(crdt, {:delete, key, module}) do
+    {map, _} = KVSet.delete(crdt, key, module)
+    map
+  end
+  def apply(crdt, {:get, key, module}), do: KVSet.get(crdt, key, module)
+  def apply(crdt, {:has_key, key, module}), do: KVSet.has_key?(crdt, key, module)
+  def apply(crdt, :keys), do: KVSet.keys(crdt)
+  def apply(crdt, :value), do: KVSet.value(crdt)
+
+  @doc """
+  Joins 2 CRDT's of the same type.
+
+  2 different types cannot mix (yet).
+
+      iex> alias Loom.CRDT
+      iex> alias Loom.MVRegister, as: Reg
+      iex> {test_a,_} = Reg.new |> Reg.set(:a, 1)
+      iex> {test_b,_} = Reg.new |> Reg.set(:b, 2)
+      iex> a = Loom.AWORMap.new |> CRDT.apply({:put, :a, :test, test_a})
+      iex> b = Loom.AWORMap.new |> CRDT.apply({:put, :b, :test, test_b})
+      iex> CRDT.join(a,b) |> CRDT.apply(:value)
+      %{test: [1,2]}
+
+  """
+  def join(a, b), do: KVSet.join(a, b)
+
+  @doc """
+  Returns the most natural primitive value for a set, a list.
+
+  iex> Loom.AWORMap.new |> Loom.CRDT.value
+  nil
+
+  """
+  def value(crdt), do: KVSet.value(crdt)
 
 end
--- test/loom_awormap_test.exs
+++ test/loom_awormap_test.exs
@@ -2,21 +2,24 @@ defmodule LoomAwormapTest do
   use ExUnit.Case
   use Loom
 
-  test "Basic add" do
+  doctest Loom.AWORMap
+  doctest Loom.CRDT.Loom.AWORMap # The Protocol implementation
+
+  test "Basic put" do
     ctr = GCounter.new
-    {map, _} = KVSet.new |> KVSet.add(:a, 1, ctr)
-    assert ctr == KVSet.get(map, 1, GCounter)
-    assert nil == KVSet.get(map, 3, GCounter)
+    {map, _} = KVSet.new |> KVSet.put(:a, 1, ctr)
+    assert 0 == KVSet.get(map, 1, GCounter)
+    assert 0 == KVSet.get(map, 3, GCounter)
     assert true == KVSet.has_key?(map, 1, GCounter)
     assert false == KVSet.has_key?(map, 2, GCounter)
-    assert %{ {1,GCounter} => ctr } == map |> KVSet.value
+    assert %{ 1 => 0 } == map |> KVSet.value
   end
 
-  test "Basic add and remove" do
+  test "Basic put and delete" do
     ctr = GCounter.new
-    {map, _} = KVSet.new |> KVSet.add(:a, 1, ctr)
-    assert ctr == KVSet.get(map, 1, ctr)
-    {map2, _} = KVSet.remove(map, 1, ctr)
+    {map, _} = KVSet.new |> KVSet.put(:a, 1, ctr)
+    assert 0 == KVSet.get(map, 1, ctr)
+    {map2, _} = KVSet.delete(map, 1, ctr)
     assert true == KVSet.has_key?(map, 1, GCounter)
     assert false == KVSet.has_key?(map, 2, GCounter)
     assert false == KVSet.has_key?(map2, 1, GCounter)
@@ -26,10 +29,10 @@ defmodule LoomAwormapTest do
     ctr = GCounter.new |> GCounter.inc(:a, 5)
     ctr1  = ctr |> GCounter.inc(:b, 10)
     {map,_} = KVSet.new
-        |> KVSet.add(:a, 1, ctr)
-        |> KVSet.add(:a, 1, ctr)
-        |> KVSet.add(:b, 1, ctr1)
-        |> KVSet.add(:a, 3, ctr)
+        |> KVSet.put(:a, 1, ctr)
+        |> KVSet.put(:a, 1, ctr)
+        |> KVSet.put(:b, 1, ctr1)
+        |> KVSet.put(:a, 3, ctr)
     assert ctr1 != ctr
     assert [{1,GCounter},{3,GCounter}] == KVSet.keys(map) |> Enum.sort
   end
@@ -38,9 +41,9 @@ defmodule LoomAwormapTest do
     ctrA = GCounter.new |> GCounter.inc(:a, 10)
     ctrB = GCounter.new |> GCounter.inc(:b, 10)
     ctrC = GCounter.join(ctrA, ctrB)
-    {setA, deltaA} = KVSet.new |> KVSet.add(:a, "counter", ctrA) |> KVSet.add(:a, "lft", ctrA)
-    {setB, deltaB} = KVSet.new |> KVSet.add(:b, "counter", ctrB) |> KVSet.add(:b, "rgt", ctrB)
-    expected = %{{"counter",GCounter} => ctrC, {"lft",GCounter} => ctrA, {"rgt",GCounter} => ctrB}
+    {setA, deltaA} = KVSet.new |> KVSet.put(:a, "counter", ctrA) |> KVSet.put(:a, "lft", ctrA)
+    {setB, deltaB} = KVSet.new |> KVSet.put(:b, "counter", ctrB) |> KVSet.put(:b, "rgt", ctrB)
+    expected = %{"counter" => Loom.CRDT.value(ctrC), "lft" => 10, "rgt" => 10}
     assert expected == KVSet.join(setA, setB) |> KVSet.value
     assert expected == KVSet.join(setA, deltaB) |> KVSet.value
     assert expected == KVSet.join(deltaA, setB) |> KVSet.value
