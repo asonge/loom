3aefbf02dd7b051d00ef9f9cf04e4f99a209302e
Fernando Mendes <devfrmendes@gmail.com>
2017-04-07 11:44:58 +0100

    Remove describe block

--- test/loom_lwwregister_test.exs
+++ test/loom_lwwregister_test.exs
@@ -10,39 +10,37 @@ defmodule LoomLwwregisterTest do
     assert "test" == reg |> Reg.value
   end
 
-  describe "Join" do
-    test "with default clock" do
-      a = Reg.new("a")
-      b = Reg.new("b")
-      ab_value = a |> Reg.join(b) |> Reg.value
-      assert "b" == ab_value
-    end
+  test "Join with default clock" do
+    a = Reg.new("a")
+    b = Reg.new("b")
+    ab_value = a |> Reg.join(b) |> Reg.value
+    assert "b" == ab_value
+  end
 
-    test "with explicit clocks" do
-      a = Reg.new("a", 2)
-      b = Reg.new("b", 1)
-      ab_value = a |> Reg.join(b) |> Reg.value
-      assert "a" == ab_value
-    end
+  test "Join with explicit clocks" do
+    a = Reg.new("a", 2)
+    b = Reg.new("b", 1)
+    ab_value = a |> Reg.join(b) |> Reg.value
+    assert "a" == ab_value
+  end
 
-    test "with equal crdt" do
-      a = Reg.new("a")
-      value = a |> Reg.join(a) |> Reg.value
-      assert "a" == value
-    end
+  test "Join with equal crdt" do
+    a = Reg.new("a")
+    value = a |> Reg.join(a) |> Reg.value
+    assert "a" == value
+  end
 
-    test "with equal clocks" do
-      a = Reg.new("a", 1)
-      b = Reg.new("b", 1)
-      ab_value = a |> Reg.join(b) |> Reg.value
-      assert "b" == ab_value
-    end
+  test "Join with equal clocks" do
+    a = Reg.new("a", 1)
+    b = Reg.new("b", 1)
+    ab_value = a |> Reg.join(b) |> Reg.value
+    assert "b" == ab_value
+  end
 
-    test "with nil clock" do
-      a = Reg.new("a")
-      b = Reg.new("b", nil)
-      ab_value = a |> Reg.join(b) |> Reg.value
-      assert "a" == ab_value
-    end
+  test "Join with nil clock" do
+    a = Reg.new("a")
+    b = Reg.new("b", nil)
+    ab_value = a |> Reg.join(b) |> Reg.value
+    assert "a" == ab_value
   end
 end
